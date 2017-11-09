d06b14e527ef9a3e4bf4855cdaa86b668f92fa0e
Priyanka Pathoori <priyanka.pathoori+4@wavemaker.com>
2017-11-09 06:00:54 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "WMO_Test_url",
+  "homePage" : "Main",
+  "name" : "WMO_Test_url",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
